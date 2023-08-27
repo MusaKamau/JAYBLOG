ffcf2b6302e32b7a2df8db28a36def28aa511248
MusaKamau <141785937+MusaKamau@users.noreply.github.com>
2023-08-27 04:05:04 +0300

    Add files via upload

---
+++ conf.js
@@ -0,0 +1,10 @@
+window.appConf = {
+    author: 'The Purple Club',
+    dashboardId: '1WYSZbWkOVJ1Hw7aetftM_j-j7n2_AOg-F6lWPyPP6qU',
+    sendContactMessageUrlId:
+        'AKfycbzbSGucaLQx0fk21bXL6EPzP6GfY6Dp1FHwNoL2m_HeZyyfA7XYZLP5cOZMIi751TOoQQ',
+    shortname: 'easydrivecms',
+    root: 'home',
+    ipInfoToken: '0efb550629ccad',
+    googleApiKey: 'AIzaSyDK37LHCky4_yIggqcKAYmFoXx6mryW7GM',
+}
